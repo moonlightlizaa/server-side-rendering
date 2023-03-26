a0d89d60ee1f7fb984be57d71018d7b35eb76016
moonlightlizaa <eliza.lacroes@gmail.com>
2023-03-26 14:55:30 +0200

    update

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,5 @@
+// NAVBAR
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggler = document.querySelector(".toggler");
   const nav = document.querySelector("nav");
@@ -6,3 +8,87 @@ document.addEventListener("DOMContentLoaded", function () {
     nav.style.display = nav.style.display === "none" ? "block" : "none";
   });
 });
+
+// LOAD MORE BUTTON
+
+const methodsContainer = document.querySelector(".methods");
+const loadMoreButton = document.querySelector(".load-more-button");
+
+let methods = [];
+let methodsToShow = 12;
+let isShowingAllMethods = false;
+
+function showMethods() {
+  methodsContainer.innerHTML = "";
+
+  methods.slice(0, methodsToShow).forEach((method) => {
+    const methodHTML = `
+      <div class="method">
+        ${
+          method.template && method.template.url
+            ? `<img src="${method.template.url}" alt="Voorbeeld van ${method.title}">`
+            : `<img src="/assets/placeholder.jpg" alt="Placeholder Image">`
+        }
+        <h2>${method.title}</h2>
+      </div>
+    `;
+    methodsContainer.insertAdjacentHTML("beforeend", methodHTML);
+  });
+
+  if (methodsToShow >= methods.length) {
+    loadMoreButton.textContent = "Laad Minder";
+    isShowingAllMethods = true;
+  } else {
+    loadMoreButton.textContent = "Laad Meer";
+    isShowingAllMethods = false;
+  }
+}
+
+function getMethods() {
+  fetch("https://api.visualthinking.fdnd.nl/api/v1/methods?first=100")
+    .then((response) => response.json())
+    .then((data) => {
+      methods = data.methods;
+      showMethods();
+    })
+    .catch((error) => {
+      console.error("Error fetching methods:", error);
+    });
+}
+
+getMethods();
+
+loadMoreButton.addEventListener("click", function () {
+  if (isShowingAllMethods) {
+    methodsToShow = 12;
+    loadMoreButton.textContent = "Laad Meer";
+    isShowingAllMethods = false;
+  } else {
+    methodsToShow = methods.length;
+    loadMoreButton.textContent = "Laad Minder";
+    isShowingAllMethods = true;
+  }
+  showMethods();
+});
+
+// DROPDOWN MENU
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function categoriesMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".category-dropdown")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
