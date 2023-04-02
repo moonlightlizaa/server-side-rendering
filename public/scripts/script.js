c18411498fbfc38683cb5e2c6d1d8844c3611ae0
moonlightlizaa <eliza.lacroes@gmail.com>
2023-04-02 21:34:53 +0200

    added comments for load more

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -11,16 +11,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // LOAD MORE BUTTON
 
+// Select the elements from the DOM that we will be interacting with
 const methodsContainer = document.querySelector(".methods");
 const loadMoreButton = document.querySelector(".load-more-button");
 
+// Initialize empty arrays and variables to store data
 let methods = [];
 let methodsToShow = 12;
 let isShowingAllMethods = false;
 
+// This function generates the HTML for the methods and updates the DOM
 function showMethods() {
+  // Clear the existing methods from the DOM
   methodsContainer.innerHTML = "";
 
+  // Loop through the first `methodsToShow` number of methods in the `methods` array
+  // and generate HTML for each method
   methods.slice(0, methodsToShow).forEach((method) => {
     const methodHTML = `
       <div class="method">
@@ -32,9 +38,11 @@ function showMethods() {
         <h2>${method.title}</h2>
       </div>
     `;
+    // Add the generated HTML to the `methodsContainer` element
     methodsContainer.insertAdjacentHTML("beforeend", methodHTML);
   });
 
+  // Update the text of the `loadMoreButton` depending on whether all methods are shown
   if (methodsToShow >= methods.length) {
     loadMoreButton.textContent = "Laad Minder";
     isShowingAllMethods = true;
@@ -44,11 +52,13 @@ function showMethods() {
   }
 }
 
+// This function fetches the methods data from an API and updates the `methods` array
 function getMethods() {
   fetch("https://api.visualthinking.fdnd.nl/api/v1/methods?first=100")
     .then((response) => response.json())
     .then((data) => {
       methods = data.methods;
+      // Once the `methods` array is updated, call the `showMethods` function to display them
       showMethods();
     })
     .catch((error) => {
@@ -56,19 +66,22 @@ function getMethods() {
     });
 }
 
+// Call the `getMethods` function to start fetching and displaying the methods
 getMethods();
 
+// Add a click event listener to the `loadMoreButton` element
 loadMoreButton.addEventListener("click", function () {
   if (isShowingAllMethods) {
+    // If all methods are already shown, update `methodsToShow` to only show the first 12 methods
     methodsToShow = 12;
     loadMoreButton.textContent = "Laad Meer";
     isShowingAllMethods = false;
   } else {
+    // If not all methods are shown, update `methodsToShow` to show all available methods
     methodsToShow = methods.length;
     loadMoreButton.textContent = "Laad Minder";
     isShowingAllMethods = true;
   }
+  // Once the `methodsToShow` value is updated, call the `showMethods` function to display the methods
   showMethods();
 });
-
-// DROPDOWN MENU icon
